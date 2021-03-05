baa62f030184cc7c6230897e4b3c7fa6a4afeb94
sajen.bir <sajen.maharjan999@gmail.com>
2021-03-05 16:50:40 +0545

    changes pushed

--- models/article.js
+++ models/article.js
@@ -1,48 +1,47 @@
-const mongoose =  require('mongoose')
+const mongoose = require('mongoose')
 const marked = require('marked')
 const slugify = require('slugify')
-const createDomPurify = require('dompurify')
-const {JSDOM} =  require('jsdom')
+const createDomPurify =  require('dompurify')
+const {JSDOM} = require('jsdom')
 const dompurify = createDomPurify(new JSDOM().window)
 
-
-const articleSchema = new mongoose.Schema({
-    title : {
+const blogSchema = new mongoose.Schema({
+    title:{
         type: String,
-        required : true
+        required: true
     },
-    description:{
+    description: {
         type: String
     },
     markdown:{
         type: String,
         required: true
-    },
-    createdAt :{
+    }, 
+    createdAt:{
         type: Date,
-        default : Date.now
+        default: Date.now,
     },
     slug:{
         type: String,
-        required : true,
-        unique : true
-    },
-    sanitizedHtml: {
+        required: true,
+        unique: true
+    }, 
+    sanitizedHtml:{
         type: String,
-        required : true
+        required: true
     }
 })
 
-articleSchema.pre('validate', function (next){
-    if(this.title) {
-        this.slug = slugify(this.title, {
-            lower : true,
-            strict : true
+blogSchema.pre('validate', function(next){
+    if(this.title){
+        this.slug =  slugify(this.title,{
+            lower: true,
+            strict: true
         })
     }
     if(this.markdown){
-        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+        this.sanitizedHtml =  dompurify.sanitize(marked(this.markdown))
     }
     next()
 })
-module.exports = mongoose.model('Article', articleSchema)
+ module.exports = mongoose.model('Blog', blogSchema)
--- server.js
+++ server.js
@@ -1,30 +1,110 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 const express = require('express')
-const mongoose =  require('mongoose')
-const Article = require('./models/article')
+const app = express()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
 const articleRouter =  require('./routes/articles')
 const methodOverride = require('method-override')
-const app = express()
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users= []
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
     
-})
+}) 
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
 
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session()) 
+app.use(methodOverride('_method')) 
 
-
-app.get('/',async (req,res)=>{
-    const articles = await Article.find().sort({
-        createdAt: 'desc'
-    })
-    res.render('articles/index',{articles: articles})
+app.get('/', async(req,res)=>{
+    res.render('homepage')
 })
 
-app.use('/articles', articleRouter)
+app.use('/blog', checkAuthenticated, articleRouter)
+
+app.get('/login', checkNotAuthenticated, (req,res) =>{
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/blog',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req,res) =>{
+    res.render('register')
+})
+app.post('/register', checkNotAuthenticated, async(req,res)=>{
+try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    })
+    res.redirect('/login')
+} catch{
+    res.redirect('/register')
+}
+})
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
+  })
+
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+ function checkNotAuthenticated(req, res, next){
+     if(req, isAuthenticated()){
+        return res.redirect('/')
+     }
+     next()
+ }
+
+ function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+
+    res.redirect('/login')
+  }
+
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
 app.listen(8000)
